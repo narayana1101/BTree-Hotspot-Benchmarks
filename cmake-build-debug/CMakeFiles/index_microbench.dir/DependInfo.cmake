8dd7f738c50116264272cae751eb4e166ae39bc0
venkata <narayana.1101@gmail.com>
2020-04-06 20:02:00 -0500

    First commit Uncleaned

---
+++ cmake-build-debug/CMakeFiles/index_microbench.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/venkata/Work/cs764/benchmarks-stripped/papi_util.cpp" "/Users/venkata/Work/cs764/benchmarks-stripped/cmake-build-debug/CMakeFiles/index_microbench.dir/papi_util.cpp.o"
+  "/Users/venkata/Work/cs764/benchmarks-stripped/workload.cpp" "/Users/venkata/Work/cs764/benchmarks-stripped/cmake-build-debug/CMakeFiles/index_microbench.dir/workload.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../."
+  "../BTreeOLC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/index_microbench.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/index_microbench.dir/papi_util.cpp.o"
+  "CMakeFiles/index_microbench.dir/workload.cpp.o"
+  "index_microbench"
+  "index_microbench.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/index_microbench.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
